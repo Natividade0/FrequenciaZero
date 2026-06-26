195518e850a06f2ad6565bcc84c61f4a66e842d4
Natividade0 <86794017+Natividade0@users.noreply.github.com>
2026-06-25 23:06:24 -0300

    update play shell

--- app/src/main/java/com/frequenciazero/PlayActivity.java
+++ app/src/main/java/com/frequenciazero/PlayActivity.java
@@ -1,4 +1,13 @@
 package com.frequenciazero;
 
-public class PlayActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_restore);
+    }
 }
