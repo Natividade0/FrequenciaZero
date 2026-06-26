7a2a8381a9415f7e6034b6b97a16fa6f006753b3
Natividade0 <86794017+Natividade0@users.noreply.github.com>
2026-06-25 22:44:58 -0300

    fix: add standard main activity entry point

---
+++ app/src/main/java/com/frequenciazero/MainActivity.java
@@ -0,0 +1,10 @@
+package com.frequenciazero;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
